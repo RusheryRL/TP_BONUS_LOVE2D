c9e6906052cfe0760aa61ca4fa76dd7861b2249a
JuanPablo <juampipivetta@gmail.com>
2025-10-06 04:42:12 -0300

    add assets of egg and point earned

--- src/egg.lua
+++ src/egg.lua
@@ -1,8 +1,8 @@
 require("src.windowManagment")
 
 egg = {}
-egg.w = 50.0
-egg.h = 100.0
+egg.w = 101.0
+egg.h = 140.0
 egg.segments = 4
 egg.posX = (screenWidth / 2) - (egg.w / 2)
 egg.posY = (screenHeight / 2) - egg.h
