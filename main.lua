1e280efc94009d1dbd0a16787aaceca415dac333
JuanPablo <juampipivetta@gmail.com>
2025-10-05 23:28:07 -0300

    fix label position

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
-require("egg")
-require("windowManagment")
-require("player")
-require("gameplayManagment")
+require("src.egg")
+require("src.windowManagment")
+require("src.player")
+require("src.gameplayManagment")
 
 
 function love.load()
@@ -14,8 +14,8 @@ end
 function love.draw()
 love.graphics.rectangle("line", egg.posX, egg.posY, egg.w, egg.h, egg.segments)
 love.graphics.print("Points: ".. player.points, 1,1)
-love.graphics.print("MousePosX: ".. player.posX, 10,10)
-love.graphics.print("MousePosY: ".. player.posY, 20,20)
+love.graphics.print("MousePosX: ".. player.posX, 1,20)
+love.graphics.print("MousePosY: ".. player.posY, 1,40)
 end
 
 function love.mousepressed( x, y, button, istouch, presses )
